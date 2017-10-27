a5418db1cc770d32721ab1152dd2279700f70496
borayildiz <borayildizme@hotmail.com>
2017-10-27 13:23:31 +0200

    before MVVM

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,9 +23,10 @@ namespace WpfApplication1
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NoteToggle(object sender, RoutedEventArgs e)
         {
 
         }
