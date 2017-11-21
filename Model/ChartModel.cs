91f13554bf9aa8d5b0b6b4d0c424aa87be70a802
borayildiz <borayildizme@hotmail.com>
2017-11-21 12:50:34 +0100

    model1

--- Model/ChartModel.cs
+++ Model/ChartModel.cs
@@ -8,9 +8,27 @@ using System.Collections.ObjectModel;
 
 namespace WpfApplication1.Model{
 
+  public class Note : INotifyPropertyChanged {
+    private bool enable;
+    public bool Enable { get { return enable; } set { enable = value; RaisePropertyChanged("Enable"); } }
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void RaisePropertyChanged(string property) {
+      if (PropertyChanged != null) {
+        PropertyChanged(this, new PropertyChangedEventArgs(property));
+      }
+    }
+
+
+  }
+
+
   public class ChartModel : INotifyPropertyChanged {
-    private bool note_E;
-    public bool Note_E { get { return note_E; } set { note_E = value; RaisePropertyChanged("Note_E"); } } 
+    private Note note_A = new Note();
+    public Note Note_A { get { return note_A; } set { note_A = value; RaisePropertyChanged("Note_A"); } }
+
 
     public event PropertyChangedEventHandler PropertyChanged;
 
@@ -21,7 +39,10 @@ namespace WpfApplication1.Model{
     }
 
     public void ToggleNote(string str) {
-      note_E = true;
+      Note note = new Note();
+      note.Enable = true;
+
+      Note_A = note;
     }
 
   }
