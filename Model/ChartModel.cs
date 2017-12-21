670c5b0c53066ae211b74f26d8765b0917d560a4
borayildiz <borayildizme@hotmail.com>
2017-12-21 11:01:21 +0100

    before SaveScale

--- Model/ChartModel.cs
+++ Model/ChartModel.cs
@@ -8,6 +8,7 @@ using System.Collections.ObjectModel;
 using System.Windows.Data;
 using System.Globalization;
 
+
 namespace WpfApplication1.Model {
 
 
@@ -27,62 +28,57 @@ namespace WpfApplication1.Model {
     public const int COL_COUNT = 13;  // eg. fret
     #endregion
 
-    #region Enums
-    public enum Note {
-      C   = 0,
-      Db  = 1,
-      D   = 2,
-      Eb  = 3,
-      E   = 4,
-      F   = 5,
-      Gb  = 6,
-      G   = 7,
-      Ab  = 8,
-      A   = 9,
-      Bb  = 10,
-      B   = 11
-    }
+    #region PrivateFields
+    private int activeNoteCount;
+
+
+
+
+
+
     #endregion
 
-    private int activeNoteCount;
-    private int _rootNoteID;
-    public int RootNoteID { get { return _rootNoteID; } set { _rootNoteID = value; RaisePropertyChanged("RootNoteID"); } }
-
+    #region PublicStaticFields
     public static bool[,] EnableChart = new bool[ROW_COUNT, COL_COUNT];
     public static bool[] Notes = new bool[NOTE_COUNT];
-    public static string[] NoteStr = { "C", "Db", "D", "Eb", "E", "F", "Gb", "Ab", "A", "Bb", "B"};
+    public static string[] NoteStr = { "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B" };
+    #endregion
 
-    private bool _updateChart;
+
+    #region Properties
     private ObservableCollection<int> _rows = new ObservableCollection<int>();
-    private ObservableCollection<int> _cols = new ObservableCollection<int>();
-    private ObservableCollection<int> _notelist = new ObservableCollection<int>();
-    private int _selectedRowStart = 1, _selectedRowEnd = (ROW_COUNT - 1), _selectedColStart = 1, _selectedColEnd = (COL_COUNT - 1);
-
-
     public ObservableCollection<int> Rows { get { return _rows; } set { _rows = value; RaisePropertyChanged("Rows"); } }
-    public ObservableCollection<int> Cols { get { return _cols; } set { _cols = value; RaisePropertyChanged("Cols"); } }
-    public ObservableCollection<int> Notelist { get { return _notelist; } set { _notelist = value; RaisePropertyChanged("Notelist"); } }
 
-    public bool UpdateChart {
-      get {
-        return _updateChart;
-      }
+    private ObservableCollection<int> _cols = new ObservableCollection<int>();
+    public ObservableCollection<int> Cols { get { return _cols; } set { _cols = value; RaisePropertyChanged("Cols"); } }
+
+    private ObservableCollection<string> _notelist = new ObservableCollection<string>();
+    public ObservableCollection<string> Notelist { get { return _notelist; } set { _notelist = value; RaisePropertyChanged("Notelist"); } }
+
+    private int _rootNoteID;
+    public int RootNoteID {
+      get { return _rootNoteID; }
       set {
-        _updateChart = value;
-        RaisePropertyChanged("UpdateChart");
+        ReorderNotes((value - _rootNoteID));    // reorder selected notes by diff
+
+        _rootNoteID = value;
+        RaisePropertyChanged("RootNoteID");        
       }
     }
 
+    private int _selectedRowStart = 1;
     public int SelectedRowStart {
       get {
         return _selectedRowStart;
-      } set {
+      }
+      set {
         _selectedRowStart = value;
         SetDisplayRange(_selectedRowStart, _selectedRowEnd, _selectedColStart, _selectedColEnd);
       }
     }
 
-    public int SelectedRowEnd { 
+    private int _selectedRowEnd = (ROW_COUNT - 1);
+    public int SelectedRowEnd {
       get {
         return _selectedRowEnd;
       }
@@ -92,7 +88,8 @@ namespace WpfApplication1.Model {
       }
     }
 
-    public int SelectedColStart { 
+    private int _selectedColStart = 1;
+    public int SelectedColStart {
       get {
         return _selectedColStart;
       }
@@ -102,6 +99,7 @@ namespace WpfApplication1.Model {
       }
     }
 
+    private int _selectedColEnd = (COL_COUNT - 1);
     public int SelectedColEnd {
       get {
         return _selectedColEnd;
@@ -112,7 +110,21 @@ namespace WpfApplication1.Model {
       }
     }
 
+    private bool _updateChart;
+    public bool UpdateChart {
+      get {
+        return _updateChart;
+      }
+      set {
+        _updateChart = value;
+        RaisePropertyChanged("UpdateChart");
+      }
+    }
 
+    #endregion
+
+
+    // Constructor
     public ChartModel(){
       for(int i = 1; i < ROW_COUNT; i++) {
         _rows.Add(i);
@@ -123,21 +135,26 @@ namespace WpfApplication1.Model {
       }
 
       for (int i = 0; i < NOTE_COUNT; i++) {
-        _notelist.Add(i);
+        _notelist.Add(NoteStr[i]);
       }
 
       SetDisplayRangeAll(true);
     }
 
+
+
+
+
+    // RefreshChart
     public void RefreshChart() {
       UpdateChart = !UpdateChart;
     }
 
+    // ToggleNote
     public void ToggleNote(string str) {
       int id = Convert.ToInt32(str.Split(':')[0]);      // zero based id
       int row = Convert.ToInt32(str.Split(':')[1]);     // one based row(e.g guitar string)
       int col = Convert.ToInt32(str.Split(':')[2]);     // zero based col(e.g guitar fret)
-      int nextID;
 
       if (!Notes[id]) {
         if (activeNoteCount == 0) RootNoteID = id;      // assign root
@@ -148,31 +165,29 @@ namespace WpfApplication1.Model {
         Notes[id] = false;
         activeNoteCount--;
 
-        // Reassign root
+        // root removed, all selected notes will be removed
         if(id == RootNoteID) {
-          for(int i = 1; i < NOTE_COUNT; i++) {
-            nextID = (id + i + NOTE_COUNT) % NOTE_COUNT;
-            if (Notes[nextID] == true) {
-              RootNoteID = nextID;
-              break;
-            }
-
-          }
+          Array.Clear(Notes, 0, Notes.Length);
+          activeNoteCount = 0;
+          RootNoteID = 0;
         }
       }
+
       RefreshChart();
     }
 
-
+    // Set Display Range
     public void SetDisplayRange(int startRow, int endRow, int startCol, int endCol) {
       for(int i = 0; i < ROW_COUNT; i++) {
         for(int j = 0; j < COL_COUNT; j++) {
           EnableChart[i, j] = (i >= startRow && i <= endRow && j >= startCol && j <= endCol) ? true : false;
         }
       }
+
       RefreshChart();
     }
 
+    // Set Display Range All
     public void SetDisplayRangeAll(bool state) {
       for (int i = 0; i < ROW_COUNT; i++) {
         for (int j = 0; j < COL_COUNT; j++) {
@@ -181,14 +196,36 @@ namespace WpfApplication1.Model {
       }
     }
 
+    // Reorder selected notes
+    private void ReorderNotes(int diff) {
+      int j;
+      bool[] NotesBuf = new bool[NOTE_COUNT];
+
+      Array.Copy(Notes, NotesBuf, Notes.Length);
+      Array.Clear(Notes, 0, Notes.Length);
+
+      for (int i = 0; i < NotesBuf.Length; i++) {
+        if (NotesBuf[i]) {
+          j = (i + diff + NOTE_COUNT) % NOTE_COUNT;
+          Notes[j] = true;
+        }
+      }
+
+      RefreshChart();
+    }
+
+
+
     public void SaveScale() {
-      int id;
+      int j;
+      int diff = _rootNoteID;
       string strScale;
 
-      for (int i = RootNoteID; i < NOTE_COUNT; i++) {
-        id = (RootNoteID + i + NOTE_COUNT) % NOTE_COUNT;
-        if (Notes[id] == true) {
-          strScale = id + ":";
+      // Set scale string
+      for (int i = 0; i < Notes.Length; i++) {    // reorder selected notes by taking RootId as zero
+        if (Notes[i]) {
+          j = (i + diff + NOTE_COUNT) % NOTE_COUNT;
+          strScale = j + ":";
         }
       }
     }
